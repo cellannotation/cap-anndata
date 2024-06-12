ceab48990a1bffd33643ca4e7e936c7eb0eb48c1
Roman Mukhin <59999203+rm1113@users.noreply.github.com>
2024-06-12 19:05:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='cap_anndata',
-    version='0.1.1',
+    version='0.2.0',
     author='R. Mukhin, A. Isaev',
     author_email='roman@ebookapplications.com',
     packages=find_packages(exclude=["test"]),
-    description='Partial read of AnnData files for low-memory operations with large datasets.',
+    description='Partial read/write of AnnData (h5ad) files for low-memory operations with large datasets.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/cellannotation/cap-anndata',
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         "numpy>=1.26.3",
         "pandas>=2.2.0",
-        "anndata>=0.10.5"
+        "anndata>=0.10.5",
     ],
     extras_require={"dev": ["pytest>=8.0.0"]}
 )
