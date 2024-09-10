39d25e65307f3b5fb05fd762d29b2a4708ceb02c
Roman Mukhin <59999203+rm1113@users.noreply.github.com>
2024-09-10 15:05:06 +0200

    update read unit test

--- test/test_reader.py
+++ test/test_reader.py
@@ -3,23 +3,57 @@ import os
 
 from cap_anndata.reader import read_h5ad, read_directly
 from test.context import get_base_anndata
+import pytest
 
 
-def test_read_anndata_file():
+def prepare_h5ad_file(name):
     adata = get_base_anndata()
     temp_folder = tempfile.mkdtemp()
-    file_path = os.path.join(temp_folder, "test_read_anndata_file.h5ad")
+    file_path = os.path.join(temp_folder, name)
     adata.write_h5ad(file_path)
-    del adata
+    return file_path
 
-    with read_h5ad(file_path=file_path) as cap_adata:
+
+@pytest.mark.parametrize("edit", [True, False])
+def test_read_in_context(edit):
+    file_path = prepare_h5ad_file("test_read_in_context.h5ad")
+
+    with read_h5ad(file_path=file_path, edit=edit) as cap_adata:
         assert cap_adata is not None, "CapAnnData file must be valid!"
+        cap_adata.read_obs()
+        cap_adata.read_uns()
+        if edit:
+            cap_adata.overwrite()
 
-    cap_adata = read_h5ad(file_path=file_path)
+    os.remove(file_path)
+
+
+@pytest.mark.parametrize("edit", [True, False])
+def test_read_as_function(edit):
+    file_path = prepare_h5ad_file("test_read_as_function.h5ad")
+
+    cap_adata = read_h5ad(file_path=file_path, edit=edit)
     assert cap_adata is not None, "CapAnnData file must be valid!"
+    cap_adata.read_obs()
+    cap_adata.read_uns()
+    if edit:
+        cap_adata.overwrite()
 
-    # TODO: remove deprecated function
-    cap_adata = read_directly(file_path=file_path)
+    cap_adata._file.close()  # TODO: create a file property
+    os.remove(file_path)
+
+
+@pytest.mark.parametrize("edit", [True, False])
+def test_read_directly(edit):
+    # TODO: remove deprecated function and unit test
+    file_path = prepare_h5ad_file("test_read_directly.h5ad")
+
+    cap_adata = read_directly(file_path=file_path, edit=edit)
     assert cap_adata is not None, "CapAnnData file must be valid!"
+    cap_adata.read_obs()
+    cap_adata.read_uns()
+    if edit:
+        cap_adata.overwrite()
 
+    cap_adata._file.close()  # TODO: create a file property
     os.remove(file_path)
